55ed323a90401eb9f8f9c29a5499186b7e30cefd
Hriday Bedi <hbedi56@aceproject.space>
2021-06-29 12:12:21 -0500

    browsejobs page almost done, css overflow issue

--- src/Pages/BrowseJobs.js
+++ src/Pages/BrowseJobs.js
@@ -3,19 +3,18 @@ import Navbar from '../components/Navbar/Navbar'
 import StackedList from '../components/StackedList/StackedList';
 import JobCard from '../components/JobCard/JobCard';
 import Filter from '../components/Filter/Filter';
+import JobsData from '../components/Data/JobsData';
 
 //import {API,graphqlOperation} from 'aws-amplify';
 
 export default function BrowseJobs() {
-
+    const jobsComponents = JobsData.map(job => <JobCard key={job.id} title={job.title} location={job.location} budget={job.budget} desc={job.desc} /> )
     return (
       <div className="bg-Gray">
         <Navbar />
         <Filter />
         <div className="flex flex-col h-screen items-center">
-          <JobCard />
-          <JobCard />
-          <JobCard />
+          {jobsComponents}
         </div>    
       </div>
       
