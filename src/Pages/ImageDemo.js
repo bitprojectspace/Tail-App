98284295d76769cc3c20aff57ac1d093f82ef2fa
Hriday Bedi <hbedi56@aceproject.space>
2021-06-30 14:56:28 -0500

    tried to get avatar pic working on branch

--- src/Pages/ImageDemo.js
+++ src/Pages/ImageDemo.js
@@ -1,3 +1,86 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Avatar from 'react-avatar-edit'
+import thumb from "../images/thumb.png"
+
+class Upload extends React.Component {
+
+  constructor(props) {
+    super(props)
+    const src ={thumb}
+    this.state = {
+      preview: null,
+      src
+    }
+    this.onCrop = this.onCrop.bind(this)
+    this.onClose = this.onClose.bind(this)
+  }
+
+  onClose() {
+    this.setState({preview: null})
+  }
+
+  onCrop(preview) {
+    this.setState({preview})
+  }
+
+  render () {
+    return (
+      <div>
+        <Avatar
+          width={390}
+          height={295}
+          onCrop={this.onCrop}
+          onClose={this.onClose}
+          src={this.state.src}
+        />
+        <img src={thumb} alt="Preview" />
+      </div>
+    )
+  }
+}
+export default Upload
+//ReactDOM.render(<App /> , document.getElementById('root'))
+
+
+/*
+
+              <div>
+                <label className="block text-sm font-medium text-gray-700">Photo</label>
+                <div className="mt-1 flex items-center space-x-5">
+                  <span className="inline-block h-12 w-12 rounded-full overflow-hidden bg-gray-100">
+                    <svg className="h-full w-full text-gray-300" fill="currentColor" viewBox="0 0 24 24">
+                      <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
+                    </svg>
+                  </span>
+                  <button type="button" className="bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    Change
+                  </button>
+                </div>
+              </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const React = require('react')
 class Upload extends React.Component {
   constructor(props){
@@ -14,6 +97,7 @@ class Upload extends React.Component {
   }
   render() {
     return (
+        
       <div>
         <input type="file" onChange={this.handleChange}/>
         <img src={this.state.file}/>
@@ -22,3 +106,5 @@ class Upload extends React.Component {
   }
 }
 export default Upload
+
+*/
