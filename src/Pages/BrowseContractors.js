37a0aa4f5bf42dc1bf9d12c67e2944f28572a71d
Kyle Dotullo <KyleDotullo10@gmail.com>
2021-07-23 11:53:13 -0500

    Develops code for browse contractors

--- src/Pages/BrowseContractors.js
+++ src/Pages/BrowseContractors.js
@@ -1,23 +1,40 @@
 import React from 'react';
 import Navbar from '../components/Navbar/Navbar'
 import StackedList from '../components/StackedList/StackedList';
-import ContractorCard from '../components/ContractorCard/ContractorCard';
+//import ContractorCard from '../components/ContractorCard/ContractorCard';
+import React, { useState, useEffect } from 'react';
+import { listContractors } from '../graphql/queries';
+import Amplify, { API, graphqlOperation } from 'aws-amplify';
 
 
 export default function BrowseContractors () {
+
+    const [contractors, setContractors] = useState([]); 
+
+    useEffect(() => {
+      fetchContractors();
+     }, []);
+
+    const fetchContractors = async() => {
+       try
+       {
+        const contractorData = await API.graphql(graphqlOperation(listContractors));
+        const contractorList = songData.data.listContractors.items;
+        console.log('contractor list', contractorList);
+        setContractors(contractorList);
+       }
+
+       catch(error) 
+       {
+
+       }
+    }
+
     return (
         <div className="bg-Gray">
         <Navbar />
         <div className="flex flex-col h-full items-center">
-          <ContractorCard />
-          <ContractorCard />
-          <ContractorCard />
-          <ContractorCard />
-          <ContractorCard />
-          <ContractorCard />
-          <ContractorCard />
-          <ContractorCard />
-          <ContractorCard />
+          
         </div>    
       </div>
     )
