a890bb6430ebd19bd2a5a2c88ab6c329538a45d7
Kyle Dotullo <KyleDotullo10@gmail.com>
2021-07-27 10:00:54 -0500

    Develops Job schema

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -73,3 +73,40 @@ export const listContractors = /* GraphQL */ `
     }
   }
 `;
+export const getJob = /* GraphQL */ `
+  query GetJob($id: ID!) {
+    getJob(id: $id) {
+      id
+      title
+      location
+      status
+      type
+      description
+      date
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listJobs = /* GraphQL */ `
+  query ListJobs(
+    $filter: ModelJobFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listJobs(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        title
+        location
+        status
+        type
+        description
+        date
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
